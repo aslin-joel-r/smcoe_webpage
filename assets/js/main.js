3a349e8785f2f906d670aca97e1d3acd9b9f0d75
mcsmcoe <mediacell@stellamaryscoe.edu.in>
2023-08-12 00:00:33 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -261,3 +261,21 @@
   new PureCounter();
 
 })()
+
+
+/**
+   * Preloader
+   */
+const preloader = document.querySelector('#preloader');
+if (preloader) {
+  window.addEventListener('load', () => {
+    setTimeout(() => {
+      preloader.classList.add('loaded');
+    }, 1000);
+    setTimeout(() => {
+      preloader.remove();
+    }, 2000);
+  });
+}
+
+/**
